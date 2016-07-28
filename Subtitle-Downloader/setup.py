e8ffd552998b0b895dc688be8b4204106452a955
Rbavishi <rohan.bavishi95@gmail.com>
2016-07-28 10:23:17 +0530

    Adding requirements and a bit more details to setup.py

--- Subtitle-Downloader/setup.py
+++ Subtitle-Downloader/setup.py
@@ -1,7 +1,11 @@
 from setuptools import setup
 setup(
-      name='subtitle_search',
+      name='subtitle-search',
+      description='A command-line utility to download subtitles from Subscene.com',
+      author='Rohan Bavishi',
+      author_email='rohan.bavishi95@gmail.com',
       scripts=['subtitle_search.py'],
+      install_requires=['fuzzywuzzy', 'python-Levenshtein'],
       entry_points = {
               'console_scripts':
                   ['subtitle-search=subtitle_search:subtitleSearch']             
